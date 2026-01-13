b1855a5ab268936fa5339ef71baa12163212856d
Leevi Kesänen <AD1229@student.jamk.fi>
2026-01-14 00:19:39 +0200

    final fixings

--- src/app/components/main-page/main-page.ts
+++ src/app/components/main-page/main-page.ts
@@ -23,61 +23,61 @@ export class MainPage implements OnInit {
 
   constructor(private http: HttpClient) {}
   ngOnInit() {
-    this.http.get('/auth-call-backs-component.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('auth-call-backs-component.txt', { responseType: 'text' }).subscribe((t) => {
       this.authCallBacksComponent.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/exchangeCognitoCode.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('exchangeCognitoCode.txt', { responseType: 'text' }).subscribe((t) => {
       this.exchangeCognitoCode.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/chat.service.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('chat.service.txt', { responseType: 'text' }).subscribe((t) => {
       this.chatService.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/enterroom.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('enterroom.txt', { responseType: 'text' }).subscribe((t) => {
       this.enterroom.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/viestiReaktiivisuus.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('viestiReaktiivisuus.txt', { responseType: 'text' }).subscribe((t) => {
       this.viestiReaktiivisuus.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/app.routes.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('app.routes.txt', { responseType: 'text' }).subscribe((t) => {
       this.appRoutes.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/profile.guard.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('profile.guard.txt', { responseType: 'text' }).subscribe((t) => {
       this.profileGuard.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/main-layout.component.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('main-layout.component.txt', { responseType: 'text' }).subscribe((t) => {
       this.mainLayoutComponent.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/auth.store.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('auth.store.txt', { responseType: 'text' }).subscribe((t) => {
       this.authStore.set(t);
       setTimeout(() => {
         Prism.highlightAll();
       }, 0);
     });
-    this.http.get('/auth.service.txt', { responseType: 'text' }).subscribe((t) => {
+    this.http.get('auth.service.txt', { responseType: 'text' }).subscribe((t) => {
       this.authService.set(t);
       setTimeout(() => {
         Prism.highlightAll();
